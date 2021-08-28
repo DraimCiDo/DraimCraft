4bd591c0fb292f71eecf0675343ef30db626a109
DanichMan <53317146+DraimCiDo@users.noreply.github.com>
2021-08-28 10:46:01 +0300

    [0.0.2] Создание кода конфигурационого файла

--- src/main/java/me/draimgoose/draimcraft/DraimCraft.java
+++ src/main/java/me/draimgoose/draimcraft/DraimCraft.java
@@ -2,12 +2,35 @@ package me.draimgoose.draimcraft;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.List;
+import java.util.Set;
+
 public final class DraimCraft extends JavaPlugin {
+
+    private boolean enabled;
+    private String configReloaded;
+    private String incorrectSyntax;
+    private String message;
+    private boolean blockAllItems;
+    private List<String> materialList;
+    private Boolean enableGroups;
+    private Set<String> groupNames;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        updateConfig();
+    }
 
+    private void updateConfig() {
+        enabled = getConfig().getBoolean("draimcraft.enabled");
+        configReloaded = getConfig().getString("draimcraft.config-reloaded");
+        incorrectSyntax = getConfig().getString("draimcraft.incorrect-syntax");
+        message = getConfig().getString("draimcraft.message");
+        blockAllItems = getConfig().getBoolean("draimcraft.block-all-items");
+        materialList = getConfig().getStringList("draimcraft.default-block-list");
+        enableGroups = getConfig().getBoolean("draimcraft.enable-groups");
+        groupNames = getConfig().getConfigurationSection("draimcraft.group").getKeys(false);
     }
 
     @Override
